d255b7ececf3c8b9fed24638cd9fccdaa32b1450
Gabriel Andrade <gabrielandrade.no@hotmail.com>
2022-07-16 17:24:29 -0300

    sei la

--- Assets/Scripts/Projectile/AxeProjectile.cs
+++ Assets/Scripts/Projectile/AxeProjectile.cs
@@ -9,8 +9,12 @@ public class AxeProjectile : MonoBehaviour
     public float AliveTime;
     private void OnEnable()
     {
-        Rb.AddForce(Force);
-        Debug.Log("aqui");
+        var playerPos = CharacterController.Instance.GetPlayer().transform.position;
+
+        var dir = Mathf.Sign(playerPos.x - transform.position.x);
+        var distance = Mathf.Abs(playerPos.x - transform.position.x);
+
+        Rb.AddForce(new Vector2(distance * dir * Force.x, Force.y));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,8 +30,8 @@ public class AxeProjectile : MonoBehaviour
     {
         AliveTime -= Time.deltaTime;
 
-        if(Input.GetKeyDown(KeyCode.S))
-            Rb.AddForce(Force);
+
+          
 
         if (AliveTime <= 0)
             Destroy(gameObject);
